ca1fcfc74fb07200209cae78389bacc70ec99c02
Peter Vida <petike997@gmail.com>
2020-12-03 02:43:57 +0100

    restructure test

--- test.py
+++ test.py
@@ -2,13 +2,19 @@ import unittest
 
 import days
 
-class TestDays(unittest.TestCase):
-    def test_day1(self):
-        input = list(map(int,"1721 979 366 299 675 1456".split(' ')))
+class TestDay1(unittest.TestCase):
+    input = list(map(int,"1721 979 366 299 675 1456".split(' ')))
+    day = days.Day1(input)
+
+    def test_pt1(self):
         expected = 514579
-        result = days.day1(input)
+        result = self.day.part1()[2]
         self.assertEqual(expected, result)
 
+    def test_pt2(self):
+        expected =241861950
+        result = self.day.part2()[3]
+        self.assertEqual(expected, result)
 
 if __name__ == "__main__":
     unittest.main()
