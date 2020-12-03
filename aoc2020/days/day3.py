fb31a18c968e1020b85170fb3a94d0d306430ae4
Peter Vida <petike997@gmail.com>
2020-12-03 10:59:11 +0100

    day3 pt1

--- aoc2020/days/day3.py
+++ aoc2020/days/day3.py
@@ -1,6 +1,13 @@
-class Solution:
-    def __init__(self, input):
-        pass
+from typing import List, Dict, Callable, Tuple
 
-    def part1(self):
-        pass
+class Solution:
+    input: List[str]
+    width: int
+
+    def __init__(self, input: List[str]):
+        self.input = input
+        self.width = len(input[0])
+
+    def part1(self) -> Tuple[int, str]:
+        line = "".join([ self.input[i][(i*3)%self.width] for i in range(0,len(self.input))])
+        return (line.count('#'), line)
