59e16491eb9f6057ce3efdcf35f8fff71b30206a
Peter Vida <petike997@gmail.com>
2020-12-06 00:53:49 +0100

    move unittests into folder and separate test data

--- test/test_days.py
+++ test/test_days.py
@@ -2,10 +2,11 @@ import unittest
 
 from aoc2020.days import day1, day2, day3, day4
 
+from test import testinputs
+
 
 class TestDay1(unittest.TestCase):
-    input = list(map(int, "1721 979 366 299 675 1456".split(' ')))
-    day = day1.Solution(input)
+    day = day1.Solution(testinputs.day1input)
 
     def test_pt1(self) -> None:
         expected = 514579
@@ -19,12 +20,7 @@ class TestDay1(unittest.TestCase):
 
 
 class TestDay2(unittest.TestCase):
-    input = list(
-        map(
-            lambda line: line.strip(), """1-3 a: abcde
-                                          1-3 b: cdefg
-                                          2-9 c: ccccccccc""".split('\n')))
-    day = day2.Solution(input)
+    day = day2.Solution(testinputs.day2input)
 
     def test_pt1(self) -> None:
         expected = 2
@@ -38,20 +34,7 @@ class TestDay2(unittest.TestCase):
 
 
 class TestDay3(unittest.TestCase):
-    input = list(
-        map(
-            lambda line: line.strip(), """..##.......
-                                          #...#...#..
-                                          .#....#..#.
-                                          ..#.#...#.#
-                                          .#...##..#.
-                                          ..#.##.....
-                                          .#.#.#....#
-                                          .#........#
-                                          #.##...#...
-                                          #...##....#
-                                          .#..#...#.#""".split('\n')))
-    day = day3.Solution(input)
+    day = day3.Solution(testinputs.day3input)
 
     def test_pt1(self) -> None:
         expected = 7
@@ -77,15 +60,48 @@ hgt:179cm
 
 hcl:#cfa07d eyr:2025 pid:166559648
 iyr:2011 ecl:brn hgt:59in"""
+day4InvalidInputs: str = """eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007"""
+day4ValidInputs: str = """pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719"""
 
 
 class TestDay4(unittest.TestCase):
-    day = day4.Solution(day4input.split('\n\n'))
-
     def test_pt1(self) -> None:
-        expected = 2
-        result = self.day.part1()
-        self.assertEqual(expected, result)
+        for input, expected in [
+            (testinputs.day4input, 2),
+            (testinputs.day4valid, len(testinputs.day4valid)),
+            (testinputs.day4invalid, len(testinputs.day4invalid))
+        ]:
+            day = day4.Solution(input)
+            result = day.part1()
+            self.assertEqual(expected, result)
+
+
+class TestDay5(unittest.TestCase):
+    def test_pt1(self) -> None:
+        expected = 820
 
 
 if __name__ == "__main__":
